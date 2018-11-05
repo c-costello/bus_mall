5601dbb95c52892db3ef896e4ea497ca8af11ef8
Clarice <claricostello@outlook.com>
2018-11-05 15:19:58 -0800

    forgot to commit - created getRandomIndex function and getUnqiueImage function working on render

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 //array that catches all the objects created by Products
 var products = [];
+var mainEl = document.getElementById('main-content');
 
 
 //function that creates new Products based on name and source
@@ -8,38 +9,37 @@ function Products(name, src){
   this.name = name;
   this.src = src;
   this.votes = 0;
+
+  products.push(this);
 }
 
 var tracker = {
   totalClicks: 0,
-  mainEl: document.getElementById('main-content'),
-  
-  getRandomIndex: function() {
-    var randomNumber = Math.round(Math.random()*(products.length));
+  // mainEl: document.getElementById('main-content'),
+
+  getRandomIndex: function(range) {
+    var randomNumber = Math.round(Math.random()*(range-1));
     return(randomNumber);
   },
-  getUniqueImages: function(){
-    var imageLeft = this.getRandomIndex();
-    var imageCenter = this.getRandomIndex();
-    var imageRight = this.getRandomIndex();
-    while (imageLeft === imageCenter); {
-      if (imageLeft === imageCenter){
-        imageCenter = this.getRandomIndex();
-      }
-    }
-    while (imageLeft === imageRight); {
-      if (imageLeft === imageRight){
-        imageRight = this.getRandomIndex();
-      }
-    }
-    while (imageCenter === imageRight); {
-      if (imageCenter === imageRight){
-        imageRight = this.getRandomIndex();
-      }
-    }
+  getUniqueImages: function(range){
+    var imgLeftNum = this.getRandomIndex(range);
+    var imgCenterNum = this.getRandomIndex(range);
+    var imgRightNum = this.getRandomIndex(range);
+    console.log(imgLeftNum,imgCenterNum,imgLeftNum);
+    //
+    var imageLeft = products[imgLeftNum];
+    console.log('imageLeft', imageLeft);
+    var imageCenter = products[imgCenterNum];
+    var imageRight = products[imgRightNum];
     return([imageLeft, imageCenter, imageRight]);
   },
   renderImages: function(){
+    this.getUniqueImages();
+
+    var imgSectionElCheck = document.getElementById('imgSection');
+    if (imgSectionElCheck) {
+      imgSectionElCheck.remove();
+    }
     //create elements
     var imageSectionEl = document.createElement('section');
     var imageLeftEl = document.createElement('img');
@@ -52,29 +52,57 @@ var tracker = {
     imageSectionEl.appendChild(imageRightEl);
 
     //append section
-    this.mainEl.appendChild(imageSectionEl);
+    mainEl.appendChild(imageSectionEl);
 
     //create ids
     imageSectionEl.id = 'imgSection';
     imageLeftEl.id = 'imgLeft';
     imageCenterEl.id = 'imgCenter';
     imageRightEl.id = 'imgRight';
-    
+
     //create content;
-    imageLeftEl.src(products[this.getUniqueImages()[0]]);
-    imageLeftEl.src(products[this.getUniqueImages()[1]]);
-    imageLeftEl.src(products[this.getUniqueImages()[2]]);
+    imageLeftEl.src = this.getUniqueImages(products.length)[0].src;
+    imageCenterEl.src = this.getUniqueImages(products.length)[1].src;
+    imageRightEl.src = this.getUniqueImages(products.length)[2].src;
   },
   addClickTracker: function() {
 
   },
   clickHandler: function(event) {
 
-  },
+    var imageSectionEl = document.getElementById('imgSection');
+    var imageLeftEl = document.getElementById('imgLeft');
+    var imageCenterEl = document.getElementById('imgCenter');
+    var imageRightEl = document.getElementById('imgRight');
+    imageLeftEl.addEventListener('click', function(event){
+      tracker.renderImages();
+    });
+    imageCenterEl.addEventListener('click', function(event){
+      tracker.renderImages();
+    });
+    imageRightEl.addEventListener('click', function(event){
+      tracker.renderImages();
+    });
+
+    
+
+
+
+  }, 
+  
+  
 };
 
+
+new Products('bag', './assets/bag.jpg');
+new Products('banana', './assets/banana.jpg');
+new Products('bathroom', './assets/bathroom.jpg');
+new Products('boots', './assets/boots.jpg');
+new Products('breakfast', './assets/breakfast.jpg');
+new Products('bugglegum', './assets/bubblegum.jpg');
+
 (function createProducts(){
-
+  tracker.renderImages();
+  tracker.clickHandler();
 })();
-
-new Products('bag', 'assets/bag.jpg');
+tracker;
